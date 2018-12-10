7e32d319b60b62e71993dbd105bc77763dcc11ac
CORP\dambers <daimen.ambers@capgemini.com>
2018-12-10 15:39:54 -0800

    Ignore files and update the view modal to take in the connect.php file

--- View Modal/view.php
+++ View Modal/view.php
@@ -1,10 +1,6 @@
 <?php
     //Create Connection
-    $servername = "127.0.0.1";
-    $username = "root";
-    $password = "";
-    $dbname = "phone_book";
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    include '../PHP/connect.php';
 
     $output = '';
     $sql = '';
